552d837d5449dd503a3fe074edfd0605154fdeef
Rasel Miah <rasel6041@gmail.com>
2024-08-31 22:13:00 +0100

    fixed css error

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,1 @@
-export const BASE_API_URL: string =
-  process.env.NEXT_PUBLIC_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_BASE_API_URL!
-    : process.env.NEXT_PUBLIC_DEV_BASE_API_URL || 'http://localhost:3000';
+export const BASE_API_URL=process.env.NEXT_PUBLIC_BASE_API_URL
